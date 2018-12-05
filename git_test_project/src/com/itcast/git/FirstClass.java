a5bfc36fa201926125a344fa9038377d797e9f0d
lihaining <txfeicivvv@163.com>
2018-12-05 16:18:07 +0800

    test

---
+++ git_test_project/src/com/itcast/git/FirstClass.java
@@ -0,0 +1,16 @@
+package com.itcast.git;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+    }
+
+    public void test(){
+        int i = 0 ;
+        i += 10;
+    }
+}
